cecd9710f5538e61ba0173b63a463478cff229de
Sourcery AI <>
2022-12-12 20:28:58 +0000

    'Refactored by Sourcery'

--- Eivabot/plugins/archiver.py
+++ Eivabot/plugins/archiver.py
@@ -13,8 +13,8 @@ from telethon.tl.types import DocumentAttributeVideo
 
 from . import *
 
-thumb_image_path = Config.TMP_DOWNLOAD_DIRECTORY + "/thumb_image.jpg"
-extracted = Config.TMP_DOWNLOAD_DIRECTORY + "extracted/"
+thumb_image_path = f"{Config.TMP_DOWNLOAD_DIRECTORY}/thumb_image.jpg"
+extracted = f"{Config.TMP_DOWNLOAD_DIRECTORY}extracted/"
 if not os.path.isdir(extracted):
     os.makedirs(extracted)
 
@@ -37,15 +37,15 @@ async def _(event):
                 Config.TMP_DOWNLOAD_DIRECTORY,
             )
             directory_name = downloaded_file_name
-            await edit_or_reply(event, downloaded_file_name)
+            await edit_or_reply(event, directory_name)
         except Exception as e:  # pylint:disable=C0103,W0703
             await mone.edit(str(e))
-    zipfile.ZipFile(directory_name + ".zip", "w", zipfile.ZIP_DEFLATED).write(
+    zipfile.ZipFile(f"{directory_name}.zip", "w", zipfile.ZIP_DEFLATED).write(
         directory_name
     )
     await bot.send_file(
         event.chat_id,
-        directory_name + ".zip",
+        f"{directory_name}.zip",
         caption="**Zipped!**",
         force_document=True,
         allow_cache=False,
@@ -85,15 +85,15 @@ async def _(event):
                 ),
             )
             directory_name = downloaded_file_name
-            await event.edit(downloaded_file_name)
+            await event.edit(directory_name)
         except Exception as e:  # pylint:disable=C0103,W0703
             await mone.edit(str(e))
-    zipfile.ZipFile(directory_name + ".zip", "w", zipfile.ZIP_DEFLATED).write(
+    zipfile.ZipFile(f"{directory_name}.zip", "w", zipfile.ZIP_DEFLATED).write(
         directory_name
     )
     await bot.send_file(
         event.chat_id,
-        directory_name + ".zip",
+        f"{directory_name}.zip",
         caption="Zipped By EivaBot",
         force_document=True,
         allow_cache=False,
@@ -136,19 +136,20 @@ async def _(event):
             await event.edit("creating rar archive, please wait..")
             # patoolib.create_archive(directory_name + '.7z',directory_name)
             patoolib.create_archive(
-                directory_name + ".rar", (directory_name, Config.TMP_DOWNLOAD_DIRECTORY)
+                f"{directory_name}.rar",
+                (directory_name, Config.TMP_DOWNLOAD_DIRECTORY),
             )
             # patoolib.create_archive("/content/21.yy Avrupa (1).pdf.zip",("/content/21.yy Avrupa (1).pdf","/content/"))
             await bot.send_file(
                 event.chat_id,
-                directory_name + ".rar",
+                f"{directory_name}.rar",
                 caption="rarred By EivaBot",
                 force_document=True,
                 allow_cache=False,
                 reply_to=event.message.id,
             )
             try:
-                os.remove(directory_name + ".rar")
+                os.remove(f"{directory_name}.rar")
                 os.remove(directory_name)
             except:
                 pass
@@ -160,9 +161,7 @@ async def _(event):
     elif input_str:
         directory_name = input_str
 
-        await event.edit(
-            "Local file compressed to `{}`".format(directory_name + ".rar")
-        )
+        await event.edit(f"Local file compressed to `{directory_name}.rar`")
 
 
 @bot.on(Eiva_cmd(pattern="7z ?(.*)"))
@@ -189,19 +188,20 @@ async def _(event):
             await event.edit("creating 7z archive, please wait..")
             # patoolib.create_archive(directory_name + '.7z',directory_name)
             patoolib.create_archive(
-                directory_name + ".7z", (directory_name, Config.TMP_DOWNLOAD_DIRECTORY)
+                f"{directory_name}.7z",
+                (directory_name, Config.TMP_DOWNLOAD_DIRECTORY),
             )
             # patoolib.create_archive("/content/21.yy Avrupa (1).pdf.zip",("/content/21.yy Avrupa (1).pdf","/content/"))
             await bot.send_file(
                 event.chat_id,
-                directory_name + ".7z",
+                f"{directory_name}.7z",
                 caption="7z archived By EivaBot",
                 force_document=True,
                 allow_cache=False,
                 reply_to=event.message.id,
             )
             try:
-                os.remove(directory_name + ".7z")
+                os.remove(f"{directory_name}.7z")
                 os.remove(directory_name)
             except:
                 pass
@@ -213,7 +213,7 @@ async def _(event):
     elif input_str:
         directory_name = input_str
 
-        await event.edit("Local file compressed to `{}`".format(directory_name + ".7z"))
+        await event.edit(f"Local file compressed to `{directory_name}.7z`")
 
 
 @bot.on(Eiva_cmd(pattern="tar ?(.*)"))
@@ -266,7 +266,7 @@ async def _(event):
     elif input_str:
         directory_name = input_str
 
-        await event.edit("Local file compressed to `{}`".format(output))
+        await event.edit(f"Local file compressed to `{output}`")
 
 
 async def create_archive(input_directory):
@@ -328,9 +328,7 @@ async def _(event):
         else:
             end = datetime.datetime.now()
             ms = (end - start).seconds
-            await mone.edit(
-                "Stored the zip to `{}` in {} seconds.".format(downloaded_file_name, ms)
-            )
+            await mone.edit(f"Stored the zip to `{downloaded_file_name}` in {ms} seconds.")
 
         with zipfile.ZipFile(downloaded_file_name, "r") as zip_ref:
             zip_ref.extractall(extracted)
@@ -387,7 +385,7 @@ async def _(event):
                 except Exception as e:
                     await bot.send_message(
                         event.chat_id,
-                        "{} caused `{}`".format(caption_rts, str(e)),
+                        f"{caption_rts} caused `{str(e)}`",
                         reply_to=event.message.id,
                     )
                     # some media were having some issues
@@ -421,9 +419,7 @@ async def _(event):
         else:
             end = datetime.datetime.now()
             ms = (end - start).seconds
-            await mone.edit(
-                "Stored the rar to `{}` in {} seconds.".format(downloaded_file_name, ms)
-            )
+            await mone.edit(f"Stored the rar to `{downloaded_file_name}` in {ms} seconds.")
 
         patoolib.extract_archive(downloaded_file_name, outdir=extracted)
         filename = sorted(get_lst_of_files(extracted, []))
@@ -479,7 +475,7 @@ async def _(event):
                 except Exception as e:
                     await bot.send_message(
                         event.chat_id,
-                        "{} caused `{}`".format(caption_rts, str(e)),
+                        f"{caption_rts} caused `{str(e)}`",
                         reply_to=event.message.id,
                     )
                     # some media were having some issues
@@ -496,8 +492,8 @@ async def _(event):
     mone = await event.edit("Processing ...")
     if not os.path.isdir(Config.TMP_DOWNLOAD_DIRECTORY):
         os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)
-    extracted = Config.TMP_DOWNLOAD_DIRECTORY + "extracted/"
-    thumb_image_path = Config.TMP_DOWNLOAD_DIRECTORY + "/thumb_image.jpg"
+    extracted = f"{Config.TMP_DOWNLOAD_DIRECTORY}extracted/"
+    thumb_image_path = f"{Config.TMP_DOWNLOAD_DIRECTORY}/thumb_image.jpg"
     if not os.path.isdir(extracted):
         os.makedirs(extracted)
     if event.reply_to_msg_id:
@@ -522,24 +518,24 @@ async def _(event):
             )
         with tarfile.TarFile.open(downloaded_file_name, "r") as tar_file:
             def is_within_directory(directory, target):
-                
+
                 abs_directory = os.path.abspath(directory)
                 abs_target = os.path.abspath(target)
-            
+
                 prefix = os.path.commonprefix([abs_directory, abs_target])
-                
+
                 return prefix == abs_directory
-            
+
             def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
-            
+
                 for member in tar.getmembers():
                     member_path = os.path.join(path, member.name)
                     if not is_within_directory(path, member_path):
                         raise Exception("Attempted Path Traversal in Tar File")
-            
+
                 tar.extractall(path, members, numeric_owner=numeric_owner) 
-                
-            
+
+
             safe_extract(tar_file, path=extracted)
         # tf = tarfile.open(downloaded_file_name)
         # tf.extractall(path=extracted)
